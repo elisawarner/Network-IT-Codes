3cbc864802d5ddf504a34425e90ad99697dc3d47
Elisa Warner <elisawa@umich.edu>
2020-05-01 01:20:10 -0400

    Added a Gmail sender

---
+++ config.py
@@ -0,0 +1,34 @@
+##########################################
+# Email configurations:
+# First argument specifies type of email template
+# Second and third arguments are presenters which fill in the template blanks
+# 
+# Number of arguments, 'to' field, 'subject' field, and templates can all be altered here.
+#
+# Also required is credentials.json and pip install the gmail api. In order to start,
+# visit the following site and follow Step1 and Step2: https://developers.google.com/gmail/api/quickstart/python
+# Users must have an authorized Gmail account. Add credentials.json into the folder containing this code
+#
+# E.g. code implementation:
+# Type in console
+# `python send_email.py breadth 'Morgan Oneka' 'Santhoshi Krishnan'
+#########################################
+
+from sys import argv
+
+presenter1 = argv[2]
+presenter2 = argv[3]
+to = 'elisawa@umich.edu'
+subject = 'Lab Meeting Today at 12:30'
+
+email_templates = {
+	'depth' : 'Hi all,\n\nAs a reminder, we will have a lab meeting today at 12:30pm. Presenting today will be %s on current research in the lab\
+ and %s on a Journal Club paper. Our lab meeting link is here:\n\nhttps://umich.bluejeans.com/597850455\n\n \
+Kind Regards\n' % (presenter1,presenter2),
+
+    'breadth' : 'Hi all:\n\nAs a reminder, we will have a lab meeting today at 12:30pm. Presenting today will be %s and %s on a few journal papers.\
+  Our lab meeting link is here:\n\
+  https://umich.bluejeans.com/597850455\n\n\
+Kind Regards' % (presenter1, presenter2)
+
+}
